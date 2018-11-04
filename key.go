4b94b63ed7f084b734210e8c64d579d36287f83f
beardedfoo <773275+beardedfoo@users.noreply.github.com>
2018-11-04 13:02:28 +0100

    Create key.go

---
+++ key.go
@@ -0,0 +1,30 @@
+// Package masterkey manages the creation of cryptographic subkeys from master keys.
+package masterkey
+
+import (
+	"crypto/sha256"
+	"io"
+
+	"golang.org/x/crypto/hkdf"
+)
+
+// New returns a MasterKey struct
+func New(material []byte) MasterKey {
+	return MasterKey{Material: material}
+}
+
+// MasterKey is a crypto key that rather than being used directly generates
+// subkeys that are used for different purposes
+type MasterKey struct {
+	Material []byte
+}
+
+// SubKey returns a deterministcally generated subkey of `size` bytes
+func (m MasterKey) SubKey(id string, size int) ([]byte, error) {
+	material := make([]byte, size)
+	kdf := hkdf.New(sha256.New, m.Material, nil, []byte(id))
+	if _, err := io.ReadFull(kdf, material); err != nil {
+		return nil, err
+	}
+	return material, nil
+}
